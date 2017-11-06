03414e03aaf5f4bcf7eb0755fb7289198725823c
tathagata-c <33373411+tathagata-c@users.noreply.github.com>
2017-11-06 14:51:51 -0500

    Updated with comments.

--- awsiprange.sh
+++ awsiprange.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Replace the 'path to awsiprange.txt' with the name of the file you want to store AWS ip ranges to.
 awsiprange="/path/to/awsiprange.txt"
+
+# For every IPaddress extracted from the AWS range available on the URL, check if the IP already in our destination file.
+# If not, then append/add the IP to the file.
 for ip in `curl https://ip-ranges.amazonaws.com/ip-ranges.json 2>/dev/null | grep -oP 'ip_prefix": "\K[^"]*'`
 do
         if grep -Fxq "$ip" $awsiprange
